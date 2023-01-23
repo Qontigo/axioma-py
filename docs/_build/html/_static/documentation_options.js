51edae5f9935cdf6efef7b2f206dc405f6722c2d
Smrati Hans <shans@qontigo.com>
2023-01-23 17:26:19 -0500

    updated docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.52.g60b20ba.dirty',
+    VERSION: '0+untagged.59.gde9b9c2.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
